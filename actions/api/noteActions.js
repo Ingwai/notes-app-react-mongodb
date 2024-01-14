9a9dc026b257735db5956ab11d810b70fbc09258
Ingwai <ingwai@wp.pl>
2024-01-14 16:37:39 +0100

    add get, save, update, delete note method in api and validation and error service

--- actions/api/noteActions.js
+++ actions/api/noteActions.js
@@ -1,32 +1,68 @@
 const Note = require('../../db/models/note');
 
 class NoteActions {
-	saveNote(req, res) {
-		// const newNote = new Note({ title: 'Korczak wiecznie żywy', body: 'Biografia tego żydka' });
-		// newNote.save().then(() => {
-		// 	console.log('Notatka została zapisana');
-		// });
+	async getAllNotes(req, res) {
+		let doc;
+		try {
+			doc = await Note.find({});
+		} catch (error) {
+			console.log(error);
+			return res.status(500).json({ message: error.message });
+		}
+		res.status(200).send(doc);
+	}
 
+	async getNote(req, res) {
+		let note;
+		const id = req.params.id;
+		try {
+			note = await Note.findOne({ _id: id });
+		} catch (error) {
+			console.log(error);
+			return res.status(500).json({ message: error.message });
+		}
+		res.status(200).send(note);
+	}
+
+	async saveNote(req, res) {
+		let note;
 		const title = req.body.title;
 		const body = req.body.body;
-		res.send('Notatka została stworzona!' + title + ' ' + body);
+
+		try {
+			const note = new Note({ title, body });
+			await note.save();
+		} catch (error) {
+			console.log(error);
+			return res.status(422).json({ message: error.message });
+		}
+
+		res.status(201).json(note);
 	}
 
-	getAllNotes(req, res) {
-		res.send('API działa');
-	}
-
-	getNote(req, res) {
-		res.send('Info o notatce');
-	}
-
-	updateNote(req, res) {
-		res.send('update notatki..');
-	}
-
-	deleteNote(req, res) {
+	async updateNote(req, res) {
+		let note;
+		const title = req.body.title;
+		const body = req.body.body;
 		const id = req.params.id;
-		res.send('wykasuj notatkę. ID' + id);
+		try {
+			note = await Note.findOneAndUpdate({ _id: id }, { title: title, body: body });
+		} catch (error) {
+			console.log(error);
+			return res.status(500).json({ message: error.message });
+		}
+		res.status(201).json(note);
+	}
+
+	async deleteNote(req, res) {
+		const id = req.params.id;
+		try {
+			await Note.findOneAndDelete({ _id: id });
+		} catch (error) {
+			console.log(error);
+			return res.status(500).json({ message: error.message });
+		}
+		res.sendStatus(204);
 	}
 }
 
